17f647c196fda5447bef6e21c8d9577fe0d1991f
Richard Galvez <richardalbertogalvez@gmail.com>
2025-02-11 21:20:35 -0500

    Added ReadOnly and Admin cluster roles + IAM configurations

--- terraform/developer-user.tf
+++ terraform/developer-user.tf
@@ -1,3 +1,33 @@
 resource "aws_iam_user" "developer" {
   name = "developer"
 }
+
+resource "aws_iam_policy" "developer_eks" {
+  name = "AmazonEKSDeveloperPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Action    = [
+          "eks:DescribeCluster",
+          "eks:ListClusters"
+        ]
+        Resource  = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "developer_eks" {
+  user        = aws_iam_user.developer.name
+  policy_arn  = aws_iam_policy.developer_eks.arn
+}
+
+# Users who have access keys generated will need to have them deleted before running terraform destroy
+resource "aws_eks_access_entry" "developer" {
+  cluster_name      = aws_eks_cluster.eks.name
+  principal_arn     = aws_iam_user.developer.arn
+  kubernetes_groups = ["readOnly"]
+}
