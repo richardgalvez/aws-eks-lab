2f09032f2950a9e9504af003f0cfe12e45162a3b
Richard Galvez <richardalbertogalvez@gmail.com>
2025-02-11 22:23:22 -0500

    Helm, metrics-server, and HPA example application implemented

---
+++ terraform/helm-provider.tf
@@ -0,0 +1,16 @@
+# The data resource will wait until the EKS cluster is provisioned
+data "aws_eks_cluster" "eks" {
+  name = aws_eks_cluster.eks.name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.eks.name
+}
+
+provider "helm" {
+  kubernetes {
+    host                    = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate  = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                   = data.aws_eks_cluster_auth.eks.token     # Using auth token directly is the most convenient way
+  } 
+}
